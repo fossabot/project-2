371d90c40f30c4ef59837ddc7473b4bd8c1d4136
En4orcer <70078226+En4orcer@users.noreply.github.com>
2020-08-26 16:55:24 +0100

    [LOG]
    Remove current time from console daemon logging

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -84,7 +84,7 @@ JsonValue buildLoggerConfiguration(Level level, const std::string& logfile) {
   JsonValue& consoleLogger = cfgLoggers.pushBack(JsonValue::OBJECT);
   consoleLogger.insert("type", "console");
   consoleLogger.insert("level", static_cast<int64_t>(TRACE));
-  consoleLogger.insert("pattern", "%T %L ");
+  consoleLogger.insert("pattern");
 
   return loggerConfiguration;
 }
